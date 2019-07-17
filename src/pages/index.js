b4aba2268c13bda8f660e21864b0a8630f15b191
张蕴鹏 <13150337230@163.com>
2019-07-17 16:42:30 +0800

    [新增] 首页样式确认

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,179 +1,102 @@
 import styles from './main.css';
-import { Row, Col, Card } from 'antd';
+import { Row, Col, Card, Timeline, Badge, Icon } from 'antd';
 
 export default function () {
   return (
-    <Card bordered={false} className={styles.wrapper}>
-      <div className={styles.cardWrapper}>
-        <Row  gutter={8}>
-          <Col span={8} className={styles.scroll}>
-              <Card className={styles.grayDiv}>
-                <Card
-                  style={{ marginTop: 16 }}
-                  type="inner"
-                  title="Inner Card title"
-                  extra={<p>More</p>}
-                >
-                  Inner Card content
-             </Card>
-                <Card
-                  style={{ marginTop: 16 }}
-                  type="inner"
-                  title="Inner Card title"
-                  extra={<p>More</p>}
-                >
-                  Inner Card content
-             </Card>
-                <Card
-                  style={{ marginTop: 16 }}
-                  type="inner"
-                  title="Inner Card title"
-                  extra={<p>More</p>}
-                >
-                  Inner Card content
-             </Card>
-                <Card
-                  style={{ marginTop: 16 }}
-                  type="inner"
-                  title="Inner Card title"
-                  extra={<p>More</p>}
-                >
-                  Inner Card content
-             </Card>
-                <Card
-                  style={{ marginTop: 16 }}
-                  type="inner"
-                  title="Inner Card title"
-                  extra={<p>More</p>}
-                >
-                  Inner Card content
-             </Card>
-                <Card
-                  style={{ marginTop: 16 }}
-                  type="inner"
-                  title="Inner Card title"
-                  extra={<p>More</p>}
-                >
-                  Inner Card content
-             </Card>
-                <Card
-                  style={{ marginTop: 16 }}
-                  type="inner"
-                  title="Inner Card title"
-                  extra={<p>More</p>}
-                >
-                  Inner Card content
-             </Card>
-                <Card
-                  style={{ marginTop: 16 }}
-                  type="inner"
-                  title="Inner Card title"
-                  extra={<p>More</p>}
-                >
-                  Inner Card content
-             </Card>
-                <Card
-                  style={{ marginTop: 16 }}
-                  type="inner"
-                  title="Inner Card title"
-                  extra={<p>More</p>}
-                >
-                  Inner Card content
-             </Card>
-                <Card
-                  style={{ marginTop: 16 }}
-                  type="inner"
-                  title="Inner Card title"
-                  extra={<p>More</p>}
-                >
-                  Inner Card content
-             </Card>
+    <Row gutter={8} className={styles.wrapper}>
+      <Col span={8} className={styles.item}>
+        <div className={styles.itemHeader}>
+          <div className={styles.left}>
+            <Badge
+              count={12}
+              style={{ fontSize: '20px', verticalAlign: 'middle', backgroundColor: '#fff', color: '#999' }}
+            />
+            <Icon
+              style={{ marginLeft: '5px', verticalAlign: 'middle', fontSize: '20px' }}
+              type="smile"
+              theme="twoTone"
+              twoToneColor="#52c41a" />
+          </div>
+          <div className={styles.right}>
+            <Icon
+              style={{ marginRight: '5px', verticalAlign: 'middle', fontSize: '20px', cursor: 'pointer' }}
+              type="plus-circle" />
+          </div>
+        </div>
+        <div className={styles.itemBody}>
+          <Timeline >
+            <Timeline.Item color="green">
+              <Card>
+                <p>工作内容更明确;工作规划更详细;工作划分帮助理清思路;站会让每天都有总结，及时沟通进度与问题;</p>
               </Card>
-          </Col>
-          <Col className="gutter-row" span={8}>
-            <Card title="Card title" className={styles.grayDiv}>
-              <Card
-                style={{ marginTop: 16 }}
-                type="inner"
-                title="Inner Card title"
-                extra={<p>More</p>}
-              >
-                Inner Card content
-             </Card>
-              <Card
-                style={{ marginTop: 16 }}
-                type="inner"
-                title="Inner Card title"
-                extra={<p>More</p>}
-              >
-                Inner Card content
-             </Card>
-              <Card
-                style={{ marginTop: 16 }}
-                type="inner"
-                title="Inner Card title"
-
-                extra={<p>More</p>}
-              >
-                Inner Card content
-             </Card>
-            </Card>
-          </Col>
-          <Col className="gutter-row" span={8}>
-            <Card title="Card title" className={styles.grayDiv}>
-              <Card
-                style={{ marginTop: 16 }}
-                type="inner"
-                title="Inner Card title"
-                extra={<p>More</p>}
-              >
-                Inner Card content
-             </Card>
-              <Card
-                style={{ marginTop: 16 }}
-                type="inner"
-                title="Inner Card title"
-                extra={<p>More</p>}
-              >
-                Inner Card content
-             </Card>
-              <Card
-                style={{ marginTop: 16 }}
-                type="inner"
-                title="Inner Card title"
-                extra={<p>More</p>}
-              >
-                Inner Card content
-             </Card> <Card
-                style={{ marginTop: 16 }}
-                type="inner"
-                title="Inner Card title"
-                extra={<p>More</p>}
-              >
-                Inner Card content
-             </Card>
-              <Card
-                style={{ marginTop: 16 }}
-                type="inner"
-                title="Inner Card title"
-                extra={<p>More</p>}
-              >
-                Inner Card content
-             </Card>
-              <Card
-                style={{ marginTop: 16 }}
-                type="inner"
-                title="Inner Card title"
-                extra={<p>More</p>}
-              >
-                Inner Card content
-             </Card>
-
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    </Card>
-
+            </Timeline.Item>
+            <Timeline.Item color="green">
+              <Card>
+                <p>工作内容更明确;工作规划更详细;工作划分帮助理清思路;站会让每天都有总结，及时沟通进度与问题;</p>
+              </Card>
+            </Timeline.Item>
+            <Timeline.Item color="green">
+              <Card>
+                <p>工作内容更明确;工作规划更详细;工作划分帮助理清思路;站会让每天都有总结，及时沟通进度与问题;</p>
+              </Card>
+            </Timeline.Item >
+            <Timeline.Item color="green">
+              <Card>
+                <p>工作内容更明确;工作规划更详细;工作划分帮助理清思路;站会让每天都有总结，及时沟通进度与问题;</p>
+              </Card>
+            </Timeline.Item>
+          </Timeline>
+        </div>
+      </Col>
+      <Col span={8} className={styles.item}>
+        <div className={styles.itemHeader}>
+          <div className={styles.left}>
+            <Badge
+              count={2}
+              style={{ fontSize: '20px', verticalAlign: 'middle', backgroundColor: '#fff', color: '#999' }}
+            />
+            <Icon style={{ marginLeft: '5px', verticalAlign: 'middle', fontSize: '20px' }} type="meh" theme="twoTone" />
+          </div>
+          <div className={styles.right}>
+            <Icon style={{ marginRight: '5px', verticalAlign: 'middle', fontSize: '20px', cursor: 'pointer' }} type="plus-circle" />
+          </div>
+        </div>
+        <div className={styles.itemBody}>
+          <Timeline >
+            <Timeline.Item>
+              无数据
+          </Timeline.Item>
+          </Timeline>
+        </div>
+      </Col>
+      <Col span={8} className={styles.item}>
+        <div className={styles.itemHeader}>
+          <div className={styles.left}>
+            <Badge
+              count={1}
+              style={{ fontSize: '20px', verticalAlign: 'middle', backgroundColor: '#fff', color: '#999' }}
+            />
+            <Icon
+              style={{ marginLeft: '5px', verticalAlign: 'middle', fontSize: '20px' }}
+              type="frown"
+              theme="twoTone"
+              twoToneColor="red" />
+          </div>
+          <div className={styles.right}>
+            <Icon
+              style={{ marginRight: '5px', verticalAlign: 'middle', fontSize: '20px', cursor: 'pointer' }}
+              type="plus-circle"
+            />
+          </div>
+        </div>
+        <div className={styles.itemBody}>
+          <Timeline >
+            <Timeline.Item color="red">
+              无数据
+          </Timeline.Item>
+          </Timeline>
+        </div>
+      </Col>
+    </Row>
   );
 }
