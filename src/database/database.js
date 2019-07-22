89e09c42c5afb37311e3b0311d83d817f1caaf06
张蕴鹏 <13150337230@163.com>
2019-07-22 10:58:16 +0800

    [新增] 调整 adapter

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 import RxDB from 'rxdb';
 
-RxDB.plugin(require('pouchdb-adapter-idb'));
+RxDB.plugin(require('pouchdb-adapter-node-websql'));
 
 const collections = [
   {
@@ -15,9 +15,8 @@ let dbPromise = null;
 const _create = async () => {
   console.log('DatabaseService: creating database..');
   const db = await RxDB.create({
-    name: 'feedbacktest',
-    adapter: 'idb',
-    password: 'myLongAndStupidPassword',
+    name: 'feedbackdb',
+    adapter: 'websql',
     multiInstance: false,
   });
   console.log('DatabaseService: created database');
@@ -31,6 +30,11 @@ const _create = async () => {
   // create collections
   console.log('DatabaseService: create collections');
   await Promise.all(collections.map(colData => db.collection(colData)));
+
+  // sync remote collection
+  await db.feedback.sync({
+    remote: 'http://localhost:1234/db/feedback'
+  });
 
   return db;
 };
